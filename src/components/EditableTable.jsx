d654f51b657fbd5afa68e7613d4ee2103bc3d7d1
Chanyoung <chanyoung.cedu@gmail.com>
2022-03-25 05:36:43 +0900

    feat(component): make EditableTable component and storybook

---
+++ src/components/EditableTable.jsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import { Table, Modal, Popover } from "antd";
+
+import Task from "../models/task";
+import Api from "../api";
+// import Documentation from "./Documentation";
+import { openNotification } from "./antdCustom";
+
+export default function DetailsTable({ tasks }) {
+  const [data, setData] = useState(undefined);
+  // const [isModalVisible, setIsModalVisible] = useState(false);
+
+  // const [memoTarget, setMemoTarget] = useState(undefined);
+  // const [distractionTarget, setDistractionTarget] = useState(undefined);
+  // const [modifiedData, setModifiedData] = useState(undefined);
+
+  useEffect(() => {
+    const originData = [];
+    tasks.forEach(task => {
+      originData.push({
+        key: task._id,
+        date: task.startTimestamp.toLocaleDateString(),
+        goal: Task.msToTime(+task.goalTime),
+        time: Task.msToTime(+task.playTime),
+        memo: task.memo,
+        distraction: task.distraction,
+      });
+    });
+    setData(originData);
+  }, [tasks]);
+
+  // async function saveModifiedData(type, target, data) {
+  //   console.log(type);
+  //   console.log(target)
+  //   console.log(data);
+  // }
+
+  const columns = [
+    {
+      title: "날짜",
+      dataIndex: "date",
+      width: "10%",
+    },
+    {
+      title: "목표 시간",
+      dataIndex: "goal",
+      width: "10%",
+    },
+    {
+      title: "진행 시간",
+      dataIndex: "time",
+      width: "10%",
+    },
+    {
+      title: "메모",
+      dataIndex: "memo",
+      width: "20%",
+    },
+    {
+      title: "딴짓",
+      dataIndex: "distraction",
+      width: "20%",
+    },
+    {
+      title: "수정",
+      key: "edit",
+      render: (_, record) => (
+        // <Popover
+        //   content={
+        //     <div>
+        //       <EditButton
+        //         onClick={() => {
+        //           setMemoTarget(record);
+        //           setIsModalVisible(true);
+        //         }}
+        //       >
+        //         메모
+        //       </EditButton>
+        //       <EditButton
+        //         onClick={() => {
+        //           setDistractionTarget(record);
+        //           setIsModalVisible(true);
+        //         }}
+        //       >
+        //       딴짓
+        //       </EditButton>
+        //     </div>
+        //   }
+        //   title="데이터를 선택해주세요."
+        //   trigger="click"
+        // >
+        //   <EditButton>Edit</EditButton>
+        // </Popover>
+        <EditButton onClick={() => openNotification("top", "준비중인 기능입니다.")}>Edit</EditButton>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      {/* {memoTarget ?
+        <Modal style={{height: "100px"}} visible={isModalVisible} title="메모 변경" onOk={() => saveModifiedData("memo", memoTarget, modifiedData)} onCancel={() => {setMemoTarget(undefined); setIsModalVisible(false)}}>
+          <Documentation defaultContent={memoTarget.memo} onHandleSave={(data) => setModifiedData(data)}></Documentation>
+        </Modal> :
+        <></>
+      }
+      {distractionTarget ?
+        <Modal style={{height: "100px"}} visible={isModalVisible} title="메모 변경" onOk={() => saveModifiedData("distraction", memoTarget, modifiedData)} onCancel={() => {setMemoTarget(undefined); setIsModalVisible(false)}}>
+          <Documentation defaultContent={distractionTarget.distraction} onHandleSave={(data) => setModifiedData(data)}></Documentation>
+        </Modal> :
+        <></>
+      } */}
+      <Table columns={columns} dataSource={data} />
+    </div>
+  );
+}
+
+const EditButton = styled.button`
+  color: #DA291C;
+  font-size: 14px;
+  border: none;
+  background: none;
+`
---
+++ src/stories/EditableTable.stories.jsx
@@ -0,0 +1,155 @@
+import React from "react";
+
+import EditableTable from "../components/EditableTable";
+
+export default {
+  title: "Table/EditableTable",
+  component: EditableTable,
+};
+
+const Template = (args) => <EditableTable {...args} />;
+
+export const EditableTableSample = Template.bind({});
+EditableTableSample.args = {
+  tasks: [
+    {
+        "_id": "61ddab403c02597c636ae5e7",
+        "userId": "61dbefd228da5c1326fbd57a",
+        "labelId": "61dd8db05f25014e13af718c",
+        "startTimestamp": new Date("Wed Jan 12 2022 01:07:25 GMT+0900 (한국 표준시)"),
+        "endTimestamp": new Date("Wed Jan 12 2022 01:07:28 GMT+0900 (한국 표준시)"),
+        "pauseAndRestarts": null,
+        "goalTime": 180000,
+        "playTime": 2150,
+        "memo": {
+          "blocks": [
+            {
+               "id": "oUq2g_tl8y",
+               "type": "header",
+               "data": {
+                  "text": "이곳에 작성하세요.",
+                  "level": 3
+               }
+            },
+            {
+               "id": "zbGZFPM-iI",
+               "type": "paragraph",
+               "data": {
+                  "text": "클릭하여 간단하게 작성할 수 있습니다."
+               }
+            },
+          ],
+        },
+        "distraction": null,
+        "createdAt": "2022-01-11T16:07:28.202Z",
+        "updatedAt": "2022-01-11T16:07:28.202Z"
+    },
+    {
+        "_id": "61de639bd5044a8b7c729721",
+        "userId": "61dbefd228da5c1326fbd57a",
+        "labelId": "61de6382d5044a8b7c72971d",
+        "startTimestamp": new Date("Wed Jan 12 2022 14:13:46 GMT+0900 (한국 표준시)"),
+        "endTimestamp": new Date("Wed Jan 12 2022 14:14:03 GMT+0900 (한국 표준시)"),
+        "pauseAndRestarts": [
+            new Date("Wed Jan 12 2022 14:13:59 GMT+0900 (한국 표준시)"),
+            new Date("Wed Jan 12 2022 14:14:00 GMT+0900 (한국 표준시)")
+        ],
+        "goalTime": 14644000,
+        "playTime": 15750,
+        "memo": null,
+        "distraction": null,
+        "createdAt": "2022-01-12T05:14:03.578Z",
+        "updatedAt": "2022-01-12T05:14:03.578Z"
+    },
+    {
+        "_id": "61de63d8d5044a8b7c729727",
+        "userId": "61dbefd228da5c1326fbd57a",
+        "labelId": "61de6382d5044a8b7c72971d",
+        "startTimestamp": new Date("Wed Jan 12 2022 14:14:56 GMT+0900 (한국 표준시)"),
+        "endTimestamp": new Date("Wed Jan 12 2022 14:15:04 GMT+0900 (한국 표준시)"),
+        "pauseAndRestarts": [
+            new Date("Wed Jan 12 2022 14:15:01 GMT+0900 (한국 표준시)"),
+            new Date("Wed Jan 12 2022 14:15:02 GMT+0900 (한국 표준시)"),
+            new Date("Wed Jan 12 2022 14:15:03 GMT+0900 (한국 표준시)")
+        ],
+        "goalTime": 21967000,
+        "playTime": 6200,
+        "memo": {
+          "time": 1648150661241,
+          "blocks": [
+              {
+                  "id": "oUq2g_tl8y",
+                  "type": "paragraph",
+                  "data": {
+                      "text": "클릭하여 메모를 작성할aff"
+                  }
+              },
+              {
+                  "id": "T7NypyZxfC",
+                  "type": "paragraph",
+                  "data": {
+                      "text": "afsf"
+                  }
+              },
+              {
+                  "id": "hgUuYlxTDS",
+                  "type": "paragraph",
+                  "data": {
+                      "text": "수 있습니다."
+                  }
+              },
+              {
+                  "id": "6MDF1tzRjE",
+                  "type": "header",
+                  "data": {
+                      "text": "ddd",
+                      "level": 2
+                  }
+              },
+              {
+                  "id": "kI8bVEEN9v",
+                  "type": "paragraph",
+                  "data": {
+                      "text": "ddddd"
+                  }
+              },
+              {
+                  "id": "95u_CFK5K0",
+                  "type": "checkList",
+                  "data": {
+                      "items": [
+                          {
+                              "text": "faff",
+                              "checked": false
+                          },
+                          {
+                              "text": "ffsfa",
+                              "checked": false
+                          },
+                          {
+                              "text": "sczzz",
+                              "checked": true
+                          },
+                          {
+                              "text": "zzz",
+                              "checked": false
+                          }
+                      ]
+                  }
+              },
+              {
+                  "id": "gbAQ-H94J3",
+                  "type": "paragraph",
+                  "data": {
+                      "text": "dddddd"
+                  }
+              }
+          ],
+          "version": "2.23.2"
+      },
+        "distraction": null,
+        "createdAt": "2022-01-12T05:15:04.395Z",
+        "updatedAt": "2022-01-12T05:15:04.395Z"
+    }
+  ],
+};
